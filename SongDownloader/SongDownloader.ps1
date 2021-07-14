03889e7a148a931b23622ed9d2f75d3a7d0b8853
Arian Izadi <izadi2000@gmail.com>
2021-07-13 22:01:46 -0700

    Update SongDownloader.ps1

--- SongDownloader/SongDownloader.ps1
+++ SongDownloader/SongDownloader.ps1
@@ -1,3 +1,8 @@
+<#
+Required to have 1) ffmpeg and 2) youtube-dl
+I installed them by way of https://chocolatey.org/ :) Love yall
+#>
+
 Write-Host "Welcome to Youtube Song Downloader"
 $videoID = Read-Host -Prompt "Video ID"
 $audioFormat = Read-Host -Prompt "Audio Format (MP3 = Fastest)"
